083d21a7d7c61972b106c4e3578abf3b07ed9404
Slyberr <slybergame@gmail.com>
2026-09-09 13:23:10 +0200

    begin form for feedback

--- frontend/app/pages/more/feedback.vue
+++ frontend/app/pages/more/feedback.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="flex w-full h-full justify-center items-center my-8" @submit="">
-        <UForm class="flex flex-col gap-y-10 w-[40%] h-[50%]">
+    <div class="flex flex-col w-full h-full justify-center items-center my-8" @submit="">
+        <p class="text-2xl p-4 text-muted"> Faire un retour pour améliorer le site :)</p>
+        <UForm  class="flex flex-col gap-y-10 w-[60%] h-[50%] p-8 border-2 border-muted rounded-2xl">
             <UFormField  label="Votre mail">
-                <UInput class="w-full" size="xl" type="email" />
+                <UInput placeholder="example@issue.fr" class="w-full md:w-[40%]" size="xl" type="email" />
             </UFormField>
             <UFormField label="Texte explicatif">
-                <UTextarea class="w-full" :rows="12" v-model="formData.text" size="xl"></UTextarea>
+                <UTextarea placeholder="Pas ouf le site faudrait améliorer ce truc ou ce bidule..." class="w-full" :rows="12" v-model="formData.text" size="xl"></UTextarea>
             </UFormField>
             <UFormField>
-                <USelect class="flex w-full" size="xl" :items="items"></USelect>
+                <USelect v-model="formData.type" class="flex w-full md:w-[30%] " size="xl" :items="items"></USelect>
             </UFormField>
-            <UButton type="submit" class="flex w-[40%]" label="Envoyer"/>
+            <UButton type="submit" class="flex  justify-center text-center w-20" label="Envoyer"/>
         </UForm>
     </div>
 
@@ -25,7 +26,7 @@ const items = ref(['Bug', 'UI/UX', 'Nouvelle fonctionnalité'])
 const formData = reactive<{mail : string, text : string,type : string}>({
     mail : '',
     text : '',
-    type : ''
+    type : 'Bug'
 })
 
 
